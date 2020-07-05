884d9e403b4facaf8fe731f0a112730af23e23a5
Jeffrey Xu <iamjeffx0@gmail.com>
2020-07-05 10:41:39 -0500

    Added selenium functionality and created get request to Zillow.com

---
+++ driver.py
@@ -0,0 +1,33 @@
+<<<<<<< HEAD
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+
+def main():
+    # price = float(input("Enter price limit: "))
+    # size = float(input("Enter number of acres: "))
+
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+
+    global driver
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    driver.get("https://zillow.com/homes/celina_rb/")
+
+    html_data = driver.page_source
+
+    print(html_data)
+
+    # driver.quit()
+
+if __name__ == "__main__":
+    main()
+=======
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+
+>>>>>>> 1a664bd5426f1c27e590d721ca6e8162ae1c936f
