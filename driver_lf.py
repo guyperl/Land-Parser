cbbd421f1101b090b5fd5e480d477f661d3bd0f6
Jeffrey Xu <iamjeffx0@gmail.com>
2020-07-21 13:59:30 -0500

    Added customize class for lf land listings

--- driver_lf.py
+++ driver_lf.py
@@ -7,6 +7,34 @@ import time
 import random
 
 
+class LfListing:
+    def __init__(self, address='', price=0, size=0):
+        self.address = address
+        self.price = price
+        self.size = size
+
+    def set_address(self, address=''):
+        self.address = address
+
+    def set_price(self, price=0):
+        self.price = price
+
+    def set_size(self, size=0):
+        self.size = size
+
+    def get_address(self):
+        return self.address
+
+    def get_price(self):
+        return self.price
+
+    def get_size(self):
+        return self.size
+
+    def __str__(self):
+        return "Address: " + self.address + "\nPrice: $" + str(self.price) + "\nSize(acres): " + str(self.size)
+
+
 def main():
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
